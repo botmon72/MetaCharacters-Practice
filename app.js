6d928191a3c04439af62edbeab50ca6294479581
Benjamin Cho <baeyuncho@icloud.com>
2019-02-11 01:31:12 -0600

    updated js file

--- app.js
+++ app.js
@@ -8,9 +8,11 @@ re = /hello$/i; //MUST end with 'h'
 re = /^hello$/i; //must both END and BEGIN with the SAME 'hello'
 re = /h.llo/i;  //the '.' can act as a wild card for exactly ONE character
 re = /h*llo/i;  //the '*' can act as a wild card for ANY # of chars including 0
+re = /gre?a?y/i;  //users can spell gray in two ways. 'e' and 'a' is in question. OPTIONAL CHAR
+re = /gre?a?y\?/i; //escaping characters - to actually use '?', put '\' before
 
 //String to test
-const str = 'hllo';
+const str = 'greay?';
 
 const result = re.exec(str);
 console.log(result);
